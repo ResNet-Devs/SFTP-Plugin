dc3491c88352cf69bcd359bf5e39d1f4c0b7da06
Steven Gantz <steven.p.gantz@gmail.com>
2016-05-01 15:32:32 -0400

    Added in light documentation

--- nppsftp.py
+++ nppsftp.py
@@ -1,5 +1,15 @@
+# These imports auto-inject nppsftp.py as a valid sublime plugin
+# if placed in the /Packages folder.
 import sublime, sublime_plugin
 
-class ExampleCommand(sublime_plugin.TextCommand):
+'''
+Class containing individual run commands for the entire plugin.
+'''
+class FTPCommand(sublime_plugin.TextCommand):
+	
+	'''
+	Generic run command prepends "Hello, World!" to the
+	start of a document.
+	'''
 	def run(self, edit):
 		self.view.insert(edit, 0, "Hello, World!")
